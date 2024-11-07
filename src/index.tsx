d0ea2e421f191672519392d2640ecf320b0c9c98
mohnapne <kuvaldin2012@gmail.com>
2024-11-07 16:40:35 +0100

    lesson 6

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Lesson06 from "./lessons/lesson06/Lesson06";
+import Homework03 from "./homeworks/homework03/Homework03";
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
