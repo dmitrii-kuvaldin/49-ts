d2bf78ccc3ef9ccb72323f1cf40d0c6d3ef2dc4e
mohnapne <kuvaldin2012@gmail.com>
2024-11-15 10:45:06 +0100

    lesson 12

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,21 +1,19 @@
-import { NavLink, Outlet } from "react-router-dom";
-import styles from './layout.module.css';
+import { Outlet } from "react-router-dom";
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
+import styles from './layout.module.css'
 
 export default function Layout() {
 
   return (
     <>
-      <header className={styles.header}>
-        <NavLink className={({ isActive }) => (isActive ? styles.linkActive : '')} to={"/"}>Lessons</NavLink>
-        <NavLink className={({ isActive }) => (isActive ? styles.linkActive : '')} to={"fetch-dog"}>Fetch dog</NavLink>
-        <NavLink className={({ isActive }) => (isActive ? styles.linkActive : '')} to={"hero-gallery"}>Hero gallery</NavLink>
-      </header >
-      <main>
+      {/* здесь вызов компонента header для верстки шапки сайта */}
+      <Header />
+      <main className={styles.main}>
         {/* за место компонента Outlet будут приходить переключаемые компоненты из маршрутизации */}
         <Outlet />
-
-
       </main>
+      <Footer/>
     </>
   );
 }
