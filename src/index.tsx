da8632f301b9664da401c2667cf6e5c61417a2f2
mohnapne <kuvaldin2012@gmail.com>
2024-11-15 10:04:21 +0100

    lesson 12

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,14 @@ import Layout from "./layout/Layout";
 import Lesson01 from "./lessons/lesson01/Lesson01";
 import Lesson02 from "./lessons/lesson02/Lesson02";
 import Lesson03 from "./lessons/lesson03/Lesson03";
+import Lesson04 from "./lessons/lesson04/Lesson04";
+import Lesson05 from "./lessons/lesson05/Lesson05";
+import Lesson06 from "./lessons/lesson06/Lesson06";
+import Lesson07 from "./lessons/lesson07/Lesson07";
+import Lesson08 from "./lessons/lesson08/Lesson08";
+import Lesson09 from "./lessons/lesson09/Lesson09";
+import Lesson10 from "./lessons/lesson10/Lesson10";
+import Lesson11 from "./lessons/lesson11/Lesson11";
 
 
 const root = ReactDOM.createRoot(
@@ -23,12 +31,21 @@ root.render(
       {/* в качестве корневого элемента указываем Layout */}
       <Route path="/" element={<Layout />}>
         {/* пути ниже будут приходить на место Outlet в Layout */}
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePage/>} />
         <Route path="fetch-dog" element={<FetchDog />} />
         <Route path="hero-gallery" element={<HeroGallery data={heroes} />} />
         <Route path="lesson-1" element={<Lesson01 />} />
         <Route path="lesson-2" element={<Lesson02 />} />
         <Route path="lesson-3" element={<Lesson03 />} />
+        <Route path="lesson-4" element={<Lesson04 />} />
+        <Route path="lesson-5" element={<Lesson05 />} />
+        <Route path="lesson-6" element={<Lesson06 />} />
+        <Route path="lesson-7" element={<Lesson07 />} />
+        <Route path="lesson-8" element={<Lesson08 />} />
+        <Route path="lesson-9" element={<Lesson09 />} />
+        <Route path="lesson-10" element={<Lesson10 />} />
+        <Route path="lesson-11" element={<Lesson11 />} />
+        {/* <Route path="lesson-3" element={<Lesson03 />} /> */}
       </Route>
     </Routes>
   </HashRouter>
